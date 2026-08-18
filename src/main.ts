3be6e1139972ba21c6a1b205ca34d9dbf74167d1
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2026-08-18 08:54:14 -0300

    test: cicd cloud run

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   // O '0.0.0.0' obriga o container a abrir as portas para o mundo externo
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
